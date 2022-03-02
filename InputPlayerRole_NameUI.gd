1f4ee475f560b79787eab086361b85d2b6cbd89a
KristineNunez <91007253+KristineNunez@users.noreply.github.com>
2022-03-03 07:47:46 +0800

    Update InputPlayerRole_NameUI.gd

--- InputPlayerRole_NameUI.gd
+++ InputPlayerRole_NameUI.gd
@@ -11,8 +11,7 @@ var playerName := "Merol Muspi"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	print("Received: ", playerNo) # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
