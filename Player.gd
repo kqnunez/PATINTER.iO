f5aaaf70c68624cffb7b9c35b8e02a288ba817d4
KristineNunez <91007253+KristineNunez@users.noreply.github.com>
2022-03-25 19:41:42 +0800

    Update Player.gd

--- Player.gd
+++ Player.gd
@@ -6,10 +6,12 @@ var speed = 400  # speed in pixels/sec #Adjust when needed
 var velocity = Vector2.ZERO
 var playerRole = "Vertical Defender"
 var playerName = "Player"
+var stamina = 100 #Adjust when needed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_safe_margin(0.12) # Replace with function body. #Minimizes jittering
+	set_safe_margin(0.3) # Replace with function body. #Minimizes jittering
+	get_node("../StaminaLabel").set_text(str(stamina))
 
 #Arrow keys to run
 #Based on: http://kidscancode.org/godot_recipes/2d/topdown_movement/
@@ -26,10 +28,23 @@ func get_input():
 		
 	# Make sure diagonal movement isn't faster
 	velocity = velocity.normalized() * speed
+	
+	#Alt+arrow keys to sprint; ui_alt defined as alt key in Project Settings
+	if Input.is_action_pressed('ui_alt'):
+		if stamina > 0 and (Input.is_action_pressed('ui_up') or Input.is_action_pressed('ui_down') or Input.is_action_pressed('ui_right') or Input.is_action_pressed('ui_left')):
+			velocity = 2 * velocity #Adjust constant when needed
+		else: #Stop sprinting when no stamina
+			velocity = Vector2.ZERO
+			
+	velocity = move_and_slide(velocity)
+	
+	#When sprinting, only decrease stamina if player actually moved (prevents stamina wastage when running against wall)
+	if velocity != Vector2.ZERO and Input.is_action_pressed('ui_alt'):
+		stamina -= 1
+		get_node("../StaminaLabel").set_text(str(stamina))
 
 func _physics_process(_delta):
 	get_input()
-	velocity = move_and_slide(velocity)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
