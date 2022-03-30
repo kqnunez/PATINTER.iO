8288ca218bc7544b6e5749df15bcd32b93bbbb41
Raph Y <59307599+raphdraft@users.noreply.github.com>
2022-03-30 11:52:21 +0800

    Add files via upload
    UI for lobby functionality.

---
+++ Player_Lobby.gd
@@ -0,0 +1,54 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var playerNo := 0
+var playAreaLayout := 0
+var playerRole := 0
+var playerName := "Pat Interio"
+var lobbyName := "Pat's Lobby"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$centerTitle/lobbyName.set_text(lobbyName)
+	#Hide extra labels
+	for i in range(1, 9):
+		if i > playerNo:
+			get_tree().get_root().find_node("playerLabel" + String(i), true, false).set_visible(false)
+	$centerMenu/menuButtons/roles/players1to4/playerLabel1.set_text(playerName+" RUNNER")
+	$centerMenu/menuButtons/roleSelect/inputControls/playerNameInput.set_text(playerName)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_back_pressed():
+	get_tree().change_scene("res://Create_Join_Game.tscn")
+	
+func _on_startGameButton_pressed():
+	playerName = $centerMenu/menuButtons/roleSelect/inputControls/playerNameInput.get_text()
+	if playerName == "":
+		$namePopups/noNameAlert.popup_centered()
+		return
+	for i in playerName:
+		if not(ord(i) > 47 and ord(i) < 58):
+			if not(ord(i) > 64 and ord(i) < 91):
+				if not(ord(i) > 96 and ord(i) < 123):
+					if ord(i) != 32:
+						$namePopups/illegalCharacterAlert.popup_centered()
+						return
+	SceneManager.passPlayerNoLayout(self, "res://PlayGameUI.tscn")
+
+func _on_roleSelectInput_pressed():
+	playerName = $centerMenu/menuButtons/roleSelect/inputControls/playerNameInput.get_text()
+	
+	if playerRole == 0:
+		playerRole = 1
+		$centerMenu/menuButtons/roleSelect/inputControls/roleSelectInput.set_text("DEFENDER")
+		$centerMenu/menuButtons/roles/players1to4/playerLabel1.set_text(playerName + " DEFENDER")
+	else:
+		playerRole = 0
+		$centerMenu/menuButtons/roleSelect/inputControls/roleSelectInput.set_text("RUNNER")
+		$centerMenu/menuButtons/roles/players1to4/playerLabel1.set_text(playerName + " RUNNER")
+
---
+++ Start_Exit_Game_UI.gd
@@ -0,0 +1,42 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+#const PlayerHistory = preload("res://PlayerHistory.gd")
+#const GameHistory = preload("res://GameHistory.gd")
+#const SaveLoadGame = preload("res://SaveLoadGame.gd")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var playerHistory = PlayerHistory.new()
+	var gameHistory = GameHistory.new()
+	var saveLoadGame = SaveLoadGame.new()
+	
+	print(gameHistory.getData())
+	saveLoadGame.loadData(false)
+	print(saveLoadGame.data)
+	print("Data Loaded in Start_ExitGameUI")
+	
+
+# Testing the saving of game history and player history
+
+# Testing the loading of game history and player history
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene("res://Create_Join_Game.tscn")
+
+func _on_exit_button_pressed():
+	var playerHistory = PlayerHistory.new()
+	var gameHistory = GameHistory.new()
+	var saveLoadGame = SaveLoadGame.new()
+	gameHistory._init([3,16,2022], [23,30], "Runner", 1, 2)
+	saveLoadGame.data = gameHistory.getData()
+	saveLoadGame.saveData(false)
+	get_tree().quit()
