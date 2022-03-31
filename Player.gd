7e7f63b7db2b22286b836d1b5fcd563bab7e5df3
KristineNunez <91007253+KristineNunez@users.noreply.github.com>
2022-03-31 22:24:05 +0800

    Changed some spaces to tabs
    Indents were spaces during merge conflict resolution, but other indents were tabs; led to mixed whitespace error

--- Player.gd
+++ Player.gd
@@ -19,32 +19,32 @@ func _ready():
 func get_input():
 	velocity = Vector2.ZERO
 
-  if not isGameOver:
-	  if Input.is_action_pressed('ui_up') and playerRole != "Horizontal Defender": #Horizontal defender cannot move up and down
-		  velocity.y -= 1
-	  if Input.is_action_pressed('ui_down') and playerRole != "Horizontal Defender":
-		  velocity.y += 1
-	  if Input.is_action_pressed('ui_right') and playerRole != "Vertical Defender": #Vertical defender cannot move left and right
-		  velocity.x += 1
-	  if Input.is_action_pressed('ui_left') and playerRole != "Vertical Defender":
-		  velocity.x -= 1
+	if not isGameOver:
+		if Input.is_action_pressed('ui_up') and playerRole != "Horizontal Defender": #Horizontal defender cannot move up and down
+			velocity.y -= 1
+		if Input.is_action_pressed('ui_down') and playerRole != "Horizontal Defender":
+			velocity.y += 1
+		if Input.is_action_pressed('ui_right') and playerRole != "Vertical Defender": #Vertical defender cannot move left and right
+			velocity.x += 1
+		if Input.is_action_pressed('ui_left') and playerRole != "Vertical Defender":
+			velocity.x -= 1
 		
-	  # Make sure diagonal movement isn't faster
-	  velocity = velocity.normalized() * speed
+		# Make sure diagonal movement isn't faster
+		velocity = velocity.normalized() * speed
 	
-	  #Alt+arrow keys to sprint; ui_alt defined as alt key in Project Settings
-	  if Input.is_action_pressed('ui_alt'):
-		  if stamina > 0 and (Input.is_action_pressed('ui_up') or Input.is_action_pressed('ui_down') or Input.is_action_pressed('ui_right') or Input.is_action_pressed('ui_left')):
-			  velocity = 2 * velocity #Adjust constant when needed
-		  else: #Stop sprinting when no stamina
-			  velocity = Vector2.ZERO
+		#Alt+arrow keys to sprint; ui_alt defined as alt key in Project Settings
+		if Input.is_action_pressed('ui_alt'):
+			if stamina > 0 and (Input.is_action_pressed('ui_up') or Input.is_action_pressed('ui_down') or Input.is_action_pressed('ui_right') or Input.is_action_pressed('ui_left')):
+				velocity = 2 * velocity #Adjust constant when needed
+			else: #Stop sprinting when no stamina
+				velocity = Vector2.ZERO
 			
-	  velocity = move_and_slide(velocity)
+		velocity = move_and_slide(velocity)
 	
-	  #When sprinting, only decrease stamina if player actually moved (prevents stamina wastage when running against wall)
-	  if velocity != Vector2.ZERO and Input.is_action_pressed('ui_alt'):
-		  stamina -= 1
-		  get_node("../StaminaLabel").set_text(str(stamina))
+		#When sprinting, only decrease stamina if player actually moved (prevents stamina wastage when running against wall)
+		if velocity != Vector2.ZERO and Input.is_action_pressed('ui_alt'):
+			stamina -= 1
+			get_node("../StaminaLabel").set_text(str(stamina))
 
 func _physics_process(_delta):
 	get_input()
